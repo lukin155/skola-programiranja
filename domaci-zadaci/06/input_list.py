d2f55f26e25bd668adbf2a2be8d6ada8a8e74297
Nemanja Tonic <nemanjatonic@yahoo.com>
2018-03-12 14:22:29 +0100

    Add first homework with tests (input_list)

---
+++ domaci-zadaci/06/input_list.py
@@ -0,0 +1,95 @@
+from solutions import input_list # you can adjust the file name and the function name
+
+import unittest
+import random
+
+class TestInputList(unittest.TestCase):
+    def test_simple_one_digit(self):
+        input_string = "1,2,3,4,5"
+        delimiter = ","
+
+        actual = input_list(input_string, delimiter)
+        expected = [1, 2, 3, 4, 5]
+
+        self.assertEqual(expected, actual)
+
+    def test_simple_one_digit_negative_numbers(self):
+        input_string = "1,-2,3,-4,5"
+        delimiter = ","
+
+        actual = input_list(input_string, delimiter)
+        expected = [1, -2, 3, -4, 5]
+
+        self.assertEqual(expected, actual)
+
+    def test_simple_column_delim(self):
+        input_string ="1;2;3;4;5"
+        delimiter = ";"
+
+        actual = input_list(input_string, delimiter)
+        expected = [1, 2, 3, 4, 5]
+
+        self.assertEqual(actual, expected)
+
+    def test_simple_multiple_digits(self):
+        input_string = "1,23,456,78,9"
+        delimiter = ","
+
+        actual = input_list(input_string, delimiter)
+        expected = [1, 23, 456, 78, 9]
+
+        self.assertEqual(actual, expected)
+
+    def test_single_element_one_digit(self):
+        input_string = "1"
+        delimiter = ","
+
+        actual = input_list(input_string, delimiter)
+        expected = [1]
+
+        self.assertEqual(actual, expected)
+
+    def test_single_element_multiple_digits(self):
+        input_string = "987"
+        delimiter = ","
+
+        actual = input_list(input_string, delimiter)
+        expected = [987]
+
+        self.assertEqual(actual, expected)
+
+    def test_empty_string(self):
+        input_string = ""
+        delimiter = ","
+
+        actual = input_list(input_string, delimiter)
+        expected = []
+
+        self.assertEqual(actual, expected)
+
+    def test_random_positive_numbers(self):
+        rand_list = [random.randint(0, 1e6) for i in range(100)]
+
+        delimiter = ","
+
+        input_string = delimiter.join(map(str, rand_list))
+
+        expected = rand_list
+        actual = input_list(input_string, delimiter)
+
+        self.assertEqual(actual, expected)
+
+    def test_random_positive_and_negative_numbers(self):
+        rand_list = [random.randint(-1e6, 1e6) for i in range(100)]
+
+        delimiter = ","
+
+        input_string = delimiter.join(map(str, rand_list))
+
+        expected = rand_list
+        actual = input_list(input_string, delimiter)
+
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
--- domaci-zadaci/README.md
+++ domaci-zadaci/README.md
@@ -1,3 +1,9 @@
 # Domaći zadaci
 
 Domaći zadaci su grupisani po celinama. Svaka celina sadrži jedan ili više zadataka koji odgovaraju jednoj lekciji u [direktorijumu s lekcijama](https://github.com/lukin155/skola-programiranja/).
+
+## 06 Petlje
+### input_list
+Napisati funkciju čiji je ulaz string, a izlaz lista. Namena funkcije je učitavanje liste s tastature. U ulaznom stringu, brojevi su odvojeni zapetom.  
+**Primer:**  
+1,2,3,4,5
